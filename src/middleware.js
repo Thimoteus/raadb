c38d930e118aa6fde8783dab2dcb1c8dd0e3cc5a
thimoteus <thimoteus.teknik.io>
2015-03-06 02:11:58 +0100

    first commit

---
+++ src/middleware.js
@@ -0,0 +1,76 @@
+// Reddit As A DataBase
+// Version: 0.1.0
+
+var settings = require('../settings'),
+   _ = require('molten-core');
+
+var jaraw = new Jaraw({
+   type: "script",
+   login: {
+      username: settings.username,
+      password: settings.password
+   },
+   oauth: {
+      id: settings.id,
+      secret: settings.secret
+   },
+   user_agent: settings.userAgent,
+   rate_limit: 1000
+});
+
+var getListing = function (endpt, params, cb) {
+   // Takes an endpoint, parameters and callback.
+   // The endpoint must return a 'listing' response from reddit.
+   // Returns an array of objects in the listing.
+   if (_.isType('Function', params)) {
+      cb = params;
+      params = {};
+   }
+   var listify = function (x) {
+      return _.map(function (y) { return y.data; }, JSON.parse(x).data.children);
+   };
+   var callback = function (err, res, bod) {
+      return cb(err, res, listify(bod));
+   };
+   return jaraw.get(endpt, params, callback);
+};
+
+var getDbEndpt = function (db) {
+   return '/r/' + db + '.json';
+};
+
+var createComment = function (thing, msg, cb) {
+   // Takes a `thing` object, `msg` string and `cb` function.
+   // Replies to `thing` with `msg`, then calls `cb`.
+   cb == null && (cb = _.id);
+   var endpt = '/api/comment'
+   var opts = {
+      api_type: 'json',
+      thing_id: thing.name,
+      text: msg
+   }
+   return jaraw.post(endpt, opts, cb);
+};
+
+var createSelfText = function (sr, title, body, cb) {
+   cb = null && (cb = _.id);
+   var endpt = '/api/submit';
+   var opts = {
+      api_type: 'json',
+      kind: 'self',
+      resubmit: true,
+      sendreplies: false,
+      sr: sr,
+      text: body,
+      then: 'comments',
+      title: title
+   };
+   return jaraw.post(endpt, opts, cb);
+}
+
+module.exports = {
+   getListing: getListing,
+   getDbEndpt: getDbEndpt,
+   createComment: createcomment,
+   createSelfText: createSelfText
+}
---
+++ src/raadb.js
@@ -0,0 +1,112 @@
+// Reddit As A DataBase
+// Version: 0.1.0
+
+var _ = require('molten-core'),
+   mid = require('./middleware'),
+   getDbEndpt = mid.getDbEndpt,
+   getListing = mid.getListing,
+   createComment = mid.createComment,
+   createSelfText = mid.createSelfText,
+   verbose = true;
+
+var print = function (it) {
+   console.log(it);
+   return it;
+};
+var say = function (it) {
+   if (verbose) print(it);
+};
+
+// database metafunctions
+// these are not exposed by the raadb api
+var createId = function () {
+   return Date.now().toString(36);
+};
+
+var collectionsExist = function (db, colls, cb) {
+   // Takes an array of collections and a callback.
+   // Callback is a function that takes a possible error,
+   // response object and an array.
+   var endpt = getDbEndpt(db);
+   var opts = {
+      limit: 100
+   };
+   var callback = function (err, res, listing) {
+      if (err || !res || (res.responseCode != 200) || !listing) {
+         cb(new Error('Could not contact reddit'));
+         return; //TODO: Figure out if I need to pass errs and resses up the stack
+      }
+      var f = function (x) {
+         return _.includes(colls, x.title);
+      };
+      var xs = _.filter(f, listing);
+      return cb(err, res, xs);
+   };
+   getListing(endpt, opts, callback);
+};
+
+var createCollection = function (db, coll, cb) {
+   // Takes a string `db`, string `coll` and function `cb` and creates a new
+   // collection in db, then calls cb.
+   var id = createId();
+   createSelfText(db, coll, id, cb);
+};
+
+var getCollFromStr = function (db, coll, cb) {
+   // Takes a string `db`, string `coll` and function `cb`.
+   // If `coll` doesn't refer to an existing collection, creates one,
+   // then calls itself. Otherwise, calls `cb` with possible error, response
+   // object and collection.
+   var callback = function (err, res, colls) {
+      if (err || !res || (res.responseCode != 200) || !colls) {
+         cb(new Error('Could not access collection ' + coll));
+         return;
+      }
+      if (colls.length === 0) {
+         createCollection(db, coll, getCollFromStr);
+      } else {
+         cb(err, res, colls[0]);
+      }
+   };
+   collectionsExist(db, [coll], callback);
+};
+
+// database functions
+// these are all exposed by the raadb api
+
+// Takes string `db`, string `coll`, any `doc` and function `cb`.
+// If there's a collection referred to by `coll`, will insert `doc` into
+// that collection. Otherwise, will create a collection and then do the
+// insertion, and calls the callback with the doc id.
+var insert = function (db, coll, doc, cb) {
+   cb == null && (cb = _.id);
+   var _insert = function (err, res, collection) {
+      if (err || !res || (res.responseCode != 200) || !collection) {
+         cb(new Error('Could not perform insert into ' + coll));
+         return;
+      }
+      var docId = createId();
+      var docData = _.encode64(JSON.stringify(doc));
+      var hash = _.unlines([docId, docData]);
+      comment(collection, hash);
+      cb(err, res, docId);
+   };
+   getCollFromStr(db, coll, _insert);
+};
+
+var remove = function (db, coll, doc, cb) {
+
+};
+
+var raadb = function (db) {
+   // takes a subreddit, then exposes the raadb api
+
+   raadb.prototype.insert = _.curry(insert)(db);
+   raadb.prototype.remove = _.curry(remove)(db);
+   // raadb.prototype.find = find;
+   // raadb.prototype.insert = _.curry(insert)(_db, _colls);
+   // raadb.prototype.remove = _.curry(remove)(_db, _colls);
+   // raadb.prototype.find = _.curry(find)(_db, _colls);
+};
+
+module.exports = raadb;
